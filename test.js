ce17ac37666124b74631ba333788dfd348248cfb
Ján Neščivera <jan.nescivera@gmail.com>
2015-10-18 19:05:34 +0200

    tests added

---
+++ test.js
@@ -0,0 +1,11 @@
+var assert = require('assert');
+
+describe('add', function() {
+  it('should be correct sum 1', function() {
+    assert.equal(3, 2 + 1);
+  });
+
+  it('should be correct sum 2', function() {
+    assert.equal(3, 2 + 3);
+  });
+});
